42f7512d93c4d751f3d150a6db821b036235cf33
Sujoy Das <sujoydas96>
2020-02-26 08:46:49 +0530

    added displacement/Key to the encryption technique. Decryption will also be added

--- Caesar.py
+++ Caesar.py
@@ -1,72 +1,28 @@
-#Program that demonstrates the Caesar Encryption Method
+#Program that demonstrates the Caesar Encryption 
+#Displacement refers to the key of the encryption technique. Default value = 3
+
 class Caesar:
     def __init__(self):
-        self.temp = ""
         self.cipher = ""
+        self.alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"] 
 
-    def alphabets(self,alphabet): #lookup table
-        if alphabet == "a":
-            self.temp = "D"
-        elif alphabet == "b":
-            self.temp = "E"
-        elif alphabet == "c":
-            self.temp = "F"
-        elif alphabet == "d":
-            self.temp = "G"
-        elif alphabet == "e":
-            self.temp = "H"
-        elif alphabet == "f":
-            self.temp = "I"
-        elif alphabet == "g":
-            self.temp = "J"
-        elif alphabet == "h":
-            self.temp = "K"
-        elif alphabet == "i":
-            self.temp = "L"
-        elif alphabet == "j":
-            self.temp = "M"
-        elif alphabet == "k":
-            self.temp = "N"
-        elif alphabet == "l":
-            self.temp = "O"
-        elif alphabet == "m":
-            self.temp = "P"
-        elif alphabet == "n":
-            self.temp = "Q"
-        elif alphabet == "o":
-            self.temp = "R"
-        elif alphabet == "p":
-            self.temp = "S"
-        elif alphabet == "q":
-            self.temp = "T"
-        elif alphabet == "r":
-            self.temp = "U"
-        elif alphabet == "s":
-            self.temp = "V"
-        elif alphabet == "t":
-            self.temp = "W"
-        elif alphabet == "u":
-            self.temp = "X"
-        elif alphabet == "v":
-            self.temp = "Y"
-        elif alphabet == "w":
-            self.temp = "Z"
-        elif alphabet == "x":
-            self.temp = "A"
-        elif alphabet == "y":
-            self.temp = "B"
-        elif alphabet == "z":
-            self.temp = "C"
-        elif alphabet == " ":
-            self.temp = " "
-        else:
-            self.temp = ""
-
-    def encrypt(self,message):
-        self.message = message.lower()
-        for i in range(len(message)):
-            self.alphabets(self.message[i])
-            self.cipher = self.cipher + self.temp
+    def Index_of(self,character):
+        for i in range(0,26):
+            if character == self.alphabet[i]:
+                return i
+    
+    def encrypt(self,message,displacement = 3):
+        message = message.lower()
+        message = message.replace(" ","")
+        print(message)
+        for i in range(0,len(message)):
+            index = self.Index_of(message[i])
+            if (index+displacement) >= 26:
+                index = (index + displacement) - 26
+            else:
+                index = int(index) + displacement
+            self.cipher = self.cipher + str(self.alphabet[index])
+   
 
 instance = Caesar()
 message = str(input("Enter the message here:   "))
